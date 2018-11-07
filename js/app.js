0457690e14f32a61fcd0bb8b7a0c4c4dafebf727
Brian Bycynski <high5bri@hotmail.com>
2018-11-07 04:42:00 -0500

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,65 @@
+// Filter Buttons
+(function() {
+    // Select All Buttons
+    const filterBtn = document.querySelectorAll('.filter-btn');
+    // console.log(filterBtn);
+
+    filterBtn.forEach(function(btn) {
+        btn.addEventListener('click', function(event) {
+            // Prevent Default Action
+            event.preventDefault();
+            const value = event.target.dataset.filter;
+            // console.log(value);
+            const items = document.querySelectorAll('.store-item');
+            // console.log(items);
+
+            items.forEach(function(item) {
+                if (value === 'all') {
+                    item.style.display = 'block';
+                } else {
+                    if (item.classList.contains(value)) {
+                        item.style.display = 'block';
+                    } else {
+                        item.style.display = 'none';
+                    }
+                }
+            });
+        });
+    });
+})();
+
+// Search Input
+(function() {
+    // Target Search Box
+    const search = document.querySelector('#search-item');
+
+    search.addEventListener('keyup', function() {
+        let value = search.value.toLowerCase().trim();
+        // console.log(value);
+
+        const items = document.querySelectorAll('.store-item');
+
+        items.forEach(function(item) {
+            let type = item.dataset.item;
+
+            // console.log(type);
+            // if (type.includes(value)) {
+            //     item.style.display = 'block';
+            // } else {
+            //     item.style.display = 'none';
+            // }
+            let length = value.length;
+
+            let match = type.slice(0, length);
+
+            // console.log(value);
+            // console.log(match);
+
+            if (value === match) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    });
+})();
